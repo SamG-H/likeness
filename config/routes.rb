df0c1ec2154d417ac9e1c4c1e8da8ea6bf5ea61b
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-05-09 15:49:05 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users, only: [:index, :show]
+  
+  #auth
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
 end
